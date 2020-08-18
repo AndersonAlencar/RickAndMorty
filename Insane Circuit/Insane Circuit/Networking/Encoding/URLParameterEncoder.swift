689030852edf372d0abcf058347b8bdc12309e6d
Anderson Alencar <andersonalencar@MacBook-Pro-de-Anderson.local>
2020-08-18 11:53:11 -0300

    Implemented network request first screen

---
+++ Insane Circuit/Insane Circuit/Networking/Encoding/URLParameterEncoder.swift
@@ -0,0 +1,9 @@
+//
+//  URLParameterEncoder.swift
+//  Insane Circuit
+//
+//  Created by Anderson Alencar on 17/08/20.
+//  Copyright © 2020 Anderson Alencar. All rights reserved.
+//
+
+import Foundation
---
+++ Insane Circuit/Insane Circuit/Networking/Manager/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Insane Circuit
+//
+//  Created by Anderson Alencar on 18/08/20.
+//  Copyright © 2020 Anderson Alencar. All rights reserved.
+//
+
+import Foundation
